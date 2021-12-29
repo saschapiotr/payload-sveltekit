deaa8c137cfc12000f4e2ed080f9840584590c4a
saschapiot <piotr@immerda.ch>
2021-12-29 22:54:42 +0100

    feat(frontend): env in session

--- frontend/src/hooks.ts
+++ frontend/src/hooks.ts
@@ -2,6 +2,6 @@ import type { GetSession } from '@sveltejs/kit';
 
 export const getSession: GetSession = () => {
   return {
-    BASE_ENDPOINT: import.meta.env.VITE_BASE_ENDPOINT,
+    BASE_ENDPOINT: import.meta.env.VITE_BASE_ENDPOINT
   }
 }
--- frontend/src/routes/index.svelte
+++ frontend/src/routes/index.svelte
@@ -1,2 +1,21 @@
+<script context="module">
+  export const load = async ({page, session}) => {
+    const success = page.query.get('success') ? 'Success!' : undefined;
+    const error = page.query.get('error') ? 'Error!' : undefined;
+    return {
+      props: {
+        endpoint: session.BASE_ENDPOINT,
+        success,
+        error,
+      }
+    }
+  }
+</script>
+
+<script>
+  export let endpoint; //, success = '', error = '';
+</script>
+
 <h1>Welcome to SvelteKit</h1>
+<h2>{endpoint}</h2>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
