8eeb398fc78f71e93f4c28b9f5d342ee5b8638e8
saschapiot <piotr@immerda.ch>
2021-12-30 00:40:39 +0100

    feat(frontend): api requests

---
+++ frontend/src/lib/api.ts
@@ -0,0 +1,27 @@
+
+type Req = { method?: string, base: string, path: string, data?: string, token: string }
+type Res = Promise<{response: Response, json: string}>;
+
+export const send = async ({ method, base, path, data, token }: Req)
+  : Res => {
+
+  const opts: any = { method, headers: {} }
+
+  if(data) {
+    opts.headers['Content-Type'] = 'application/json';
+    opts.body = JSON.stringify(data);
+  }
+
+  if(token) {
+    opts.headers['Authorization'] = `Token ${token}`
+  }
+
+  const response = await fetch(encodeURI(`${base}/${path}`), opts);
+  const json: any = await response.json();
+
+  return { response, json }
+
+}
+
+export const get = ({base, path, token} : Req)
+  : Res => send({method: 'GET', base, path, token})
