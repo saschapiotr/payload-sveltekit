ed2ccb73515fbdee46e9a24d389f923e7ba91e95
saschapiot <piotr@immerda.ch>
2021-12-30 23:18:53 +0100

    feat(cms): updated posts backend and included url slugs

--- cms/src/collections/Posts.ts
+++ cms/src/collections/Posts.ts
@@ -1,10 +1,20 @@
 import { CollectionConfig, CollectionAfterChangeHook } from 'payload/types';
-import { open, editor } from '../access';
+import { open, closed, editor } from '../access';
 
 const Posts: CollectionConfig = {
   slug: 'posts',
+  labels: {
+    singular: 'Post',
+    plural: 'Posts',
+  },
   admin: {
-    useAsTitle: 'Posts',
+    useAsTitle: 'title',
+    description: 'blog posts',
+    defaultColumns: [
+      'title',
+      'url',
+      'updatedAt',
+    ]
   },
   access: {
     read: open, 
@@ -17,9 +27,27 @@ const Posts: CollectionConfig = {
       name: 'title',
       type: 'text',
     },
+    {
+      name: 'slug',
+      label: 'Url',
+      type: 'text',
+      index: true,
+      access: {
+        read: open,
+        create: closed,
+        update: closed,
+      }
+    },
   ],
   hooks: {
-    afterChange: () => {},
+    beforeChange: [
+      ((op) => { 
+        op.data.title = op.data.title.replace(/\s\s+/g, ' ');
+        op.data.slug = op.data.title.replace(/\s/g, '-').toLowerCase();
+
+        return op.data;
+      }),
+    ],
   }
 }
 
