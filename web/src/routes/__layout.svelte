94a3c12d3d867bbff5114964541da977cb9b4794
saschapiot <piotr@immerda.ch>
2021-12-31 12:08:13 +0100

    refactor(frontend): root div

--- web/src/routes/__layout.svelte
+++ web/src/routes/__layout.svelte
@@ -2,7 +2,7 @@
   import "../app.css";
 </script>
 
-<div class="bg-slate-100 h-screen">
+<div class="root bg-slate-100 h-screen">
   <nav class="w-full text-center text-2xl font-bold">
     <a class="underline pr-5" href="/">Home</a>
     <a class="underline" href="/about">About</a>
